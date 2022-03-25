7d6ce0795ca003aa3134875d337e9c6edcdcc4ec
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-03-25 13:17:20 +0200

    kirjalista toimii

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MyFunction from './MyFunction';
 import MyClass from './MyClass';
 import MyHook from './MyHook';
 import Login from './Login';
+import BookList from './BookList';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <li> <Link to="/myclass">MyClass</Link> </li>
           <li> <Link to="/myhook">MyHook</Link>  </li>
           <li> <Link to="/login">Login</Link>  </li>
+          <li> <Link to="/booklist">Kirjat</Link>  </li>
         </ul>
         <hr />
       </div>
@@ -22,6 +24,7 @@ function App() {
         <Route exact path="/myclass" element={<MyClass fname="Teppo"/>} />
         <Route exact path="/myhook" element={<MyHook fname="Teppo"/>} />
         <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/booklist" element={<BookList/>} />
       </Routes>
     </Router>
 
