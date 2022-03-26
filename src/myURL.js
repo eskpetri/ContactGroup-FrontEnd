7d4e3eadd63bcf9de07a514d146194fa8696db07
Pekka Alaluukas <Pekka.Alaluukas@mymail.com>
2022-03-26 15:04:26 +0200

    ready for heroku

--- src/myURL.js
+++ src/myURL.js
@@ -1,4 +1,4 @@
-const apiURL="https://bcamp-postgre-api.herokuapp.com/";
+const apiURL="https://bcamp-postgre-api.herokuapp.com";
 //const apiURL="https://yourappname.herokuapp.com";
 
 export default apiURL;
