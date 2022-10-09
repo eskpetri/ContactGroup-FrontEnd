6c6e16da85ea6319748e6430f23928e55fb8d465
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-10-09 16:33:34 +0300

    url edited

--- src/myURL.js
+++ src/myURL.js
@@ -1,5 +1,5 @@
 //const apiURL="https://bcamp-postgre-api.herokuapp.com";
-const apiURL="https://pekan-dotnet-university.herokuapp.com";
+const apiURL="https://pekan-dotnet-university.herokuapp.com/api";
 //const apiURL="https://localhost:7080/api";
 
 export default apiURL;
